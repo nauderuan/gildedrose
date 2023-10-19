70edb0150c4ede29b5fc441fe8ee7390b737802b
nauderuan <ruan.naude@a24group.com>
2023-10-19 21:45:51 +0200

    Added code comments to use as markers
    Added code comments to use as markers for where changes needs
    to be made

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,14 +19,16 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      // TODO first understand the existing code and add comments where needed to track what is going on 
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        // Handle the items that decrease in quality
         if (this.items[i].quality > 0) {
+          // Skip items that do not decrease or increase in value.
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
       } else {
+        // Handle the items that increase in value
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -44,12 +46,16 @@ export class GildedRose {
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        // Update sellIn for items that decrease or decrease in value
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
+        // Here we handle items that have passed their sell by date
         if (this.items[i].name != 'Aged Brie') {
+          //Handle items that decrease in quality after sell by date
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
+              // Skip items that do not decrease or increase in value after sell by date
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1
               }
@@ -58,6 +64,7 @@ export class GildedRose {
             this.items[i].quality = this.items[i].quality - this.items[i].quality
           }
         } else {
+          //Handle items that increase in quality after sell by date
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1
           }
